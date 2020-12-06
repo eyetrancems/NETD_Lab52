0cce0ac6d9b63f46c1c6d42d887b203efb9c4393
David N <dtndccp2020@outlook.com>
2020-12-06 16:07:31 -0500

    Upload final

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NETD_Lab52.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
